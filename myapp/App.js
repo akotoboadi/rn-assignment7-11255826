7da6461b4604f55194384d2ca522f4141ffe4379
AkotoB <aboadi010@st.ug.edu.gh>
2024-07-12 17:58:35 +0000

    create screens and add navigation installation

---
+++ myapp/App.js
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
+import HomeScreen from "./screens/HomeScreen";
+import MyCardsScreen from "./screens/MyCards";
+import StatisticsScreen from "./screens/Statistics";
+import SettingsScreen from "./screens/Settings";
+import { ThemeContext, ThemeProvider } from "./ThemeContext";
+import { useContext } from "react";
+import { ThemeDark, ThemeLight } from "./Data/MockupData";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <ThemeProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "MyCards") {
+                iconName = focused ? "card" : "card-outline";
+              } else if (route.name === "Statistics") {
+                iconName = focused ? "stats-chart" : "stats-chart-outline";
+              } else if (route.name === "Settings") {
+                iconName = focused ? "settings" : "settings-outline";
+              }
+
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: theme === "dark" ? "#383FFA" : "#000000", // Active tab color
+            tabBarInactiveTintColor: "gray", // Inactive tab color
+            tabBarStyle: {
+              paddingTop: 15,
+              height: 70,
+              backgroundColor:
+                theme === "dark"
+                  ? ThemeDark.backgroundColor
+                  : ThemeLight.backgroundColor, // Background color based on theme
+              borderTopWidth: 0, // Remove top border
+              elevation: 0, // Remove Android shadow
+              shadowOpacity: 0, // Remove iOS shadow
+            },
+            tabBarLabelStyle: {
+              fontWeight: "bold",
+              fontSize: 12,
+              marginBottom: 10, // Adjust the space between the icon and the text
+            },
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="MyCards" component={MyCardsScreen} />
+          <Tab.Screen name="Statistics" component={StatisticsScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
---
+++ myapp/Data/MockupData.js
@@ -0,0 +1,148 @@
+export const ThemeDark = {
+    title: "Dark",
+    fontColor: "#ffffff",
+    backgroundColor: "#161622",
+    iconContainerColor: "#1E1E2D",
+  };
+  
+  export const ThemeLight = {
+    title: "Light",
+    fontColor: "#000000",
+    backgroundColor: "#FFFFFF",
+    iconContainerColor: "#f2f2f2",
+  };
+  
+  export const TransactionsHistoryData = [
+    {
+      id: 1,
+      title: "PlayStation",
+      category: "Entertainment",
+      amount: "-$25.00",
+      type: "black",
+      logo: require("../assets/playstation.png"),
+    },
+    {
+      id: 2,
+      title: "Apple Music",
+      category: "Music",
+      amount: "-$8.99",
+      type: "black",
+      logo: require("../assets/applemusic.png"),
+    },
+    {
+      id: 3,
+      title: "Books",
+      category: "Shopping",
+      amount: "-$30.75",
+      type: "black",
+      logo: require("../assets/books.png"),
+    },
+    {
+      id: 4,
+      title: "Hulu",
+      category: "Entertainment",
+      amount: "-$10.99",
+      type: "black",
+      logo: require("../assets/hulu.png"),
+    },
+    {
+      id: 5,
+      title: "Coffee",
+      category: "Food & Drink",
+      amount: "-$4.50",
+      type: "black",
+      logo: require("../assets/coffee.png"),
+    },
+    {
+      id: 6,
+      title: "Clothing",
+      category: "Shopping",
+      amount: "-$85.00",
+      type: "black",
+      logo: require("../assets/clothing.png"),
+    },
+    {
+      id: 7,
+      title: "Lyft",
+      category: "Transport",
+      amount: "-$18.50",
+      type: "black",
+      logo: require("../assets/lyft.png"),
+    },
+    {
+      id: 9,
+      title: "Salary",
+      category: "Income",
+      amount: "+$2000.00",
+      type: "blue",
+      logo: require("../assets/salary.png"),
+    },
+  ];
+  
+  export const TransactionsHistoryDataDark = [
+    {
+      id: 1,
+      title: "PlayStation",
+      category: "Entertainment",
+      amount: "-$25.00",
+      type: "black",
+      logo: require("../assets/playstation-dark.png"),
+    },
+    {
+      id: 2,
+      title: "Apple Music",
+      category: "Music",
+      amount: "-$8.99",
+      type: "black",
+      logo: require("../assets/applemusic-dark.png"),
+    },
+    {
+      id: 3,
+      title: "Books",
+      category: "Shopping",
+      amount: "-$30.75",
+      type: "black",
+      logo: require("../assets/books-dark.png"),
+    },
+    {
+      id: 4,
+      title: "Hulu",
+      category: "Entertainment",
+      amount: "-$10.99",
+      type: "black",
+      logo: require("../assets/hulu-dark.png"),
+    },
+    {
+      id: 5,
+      title: "Coffee",
+      category: "Food & Drink",
+      amount: "-$4.50",
+      type: "black",
+      logo: require("../assets/coffee-dark.png"),
+    },
+    {
+      id: 6,
+      title: "Clothing",
+      category: "Shopping",
+      amount: "-$85.00",
+      type: "black",
+      logo: require("../assets/clothing-dark.png"),
+    },
+    {
+      id: 7,
+      title: "Lyft",
+      category: "Transport",
+      amount: "-$18.50",
+      type: "black",
+      logo: require("../assets/lyft-dark.png"),
+    },
+    {
+      id: 9,
+      title: "Salary",
+      category: "Income",
+      amount: "+$2000.00",
+      type: "blue",
+      logo: require("../assets/salary-dark.png"),
+    },
+  ];
+  
